d6ccce94a04da4fb6e475eaf918c5b04067f8ec7
krsayn <754163399@qq.com>
2020-06-16 23:04:52 +0800

    6.16 leetcode提交

---
+++ leetcode/study/src/main/easy/question217.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class question217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>(nums.length);
+        for (int num : nums) {
+            if (!set.add(num))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate1(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return true;
+        }
+        return false;
+    }
+}
---
+++ leetcode/study/src/main/easy/question628.java
@@ -0,0 +1,10 @@
+package easy;
+
+import java.util.Arrays;
+
+public class question628 {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 1] *nums[nums.length - 2] * nums[nums.length - 3]);
+    }
+}
